21d2142b4fe425ae6ecd7d66d5a6dc6200c471eb
718321790 <718321790@qq.com>
2018-10-30 10:59:45 +0800

    搜索列表一些bug修复

--- public/m/js/searchList.js
+++ public/m/js/searchList.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: lu-yijun 
+ * @Date: 2018-10-30 10:57:08 
+ * @Last Modified by: lu-yijun
+ * @Last Modified time: 2018-10-30 10:57:35
+ */
 $(function(){
 
     //获取传入的url对象
@@ -6,35 +12,58 @@ $(function(){
     var key = search.key
     $('#search').val(key)
 
-
     var data = {
         proName: key,
         page: 1 ,
         pageSize: 1000
     }
+    
     // 渲染
     function render(){
 
+        
         $.ajax({
             type: 'get',
             url: '/product/queryProduct',
             data: data,
+            beforeSend: function(){
+                $('.loading').show()
+            },
             success: function(info){
                 console.log(info)
+                
                 $('.lt-product').html(template('tmp',info))
+            },
+            complete:function(){
+                
+                $('.loading').hide()                
             }
         })
     }
+
+    // 首屏渲染
     render()
 
     // 点击搜索按钮 重新搜索商品列表
     $('.btn-search').on('click',function(){
         key = $('#search').val()
+        data = {
+            proName: key,
+            page: 1 ,
+            pageSize: 1000
+        }
         render()
     })
 
     // 点击排序栏，切换排序
     $('.lt-sort li[data-type]').on('click',function(){
+
+        data = {
+            proName: key,
+            page: 1 ,
+            pageSize: 1000
+        }
+        
         var $this = $(this)
         // 如果有active 类，点击时切换箭头方向和排序顺序
         if ($this.hasClass('active')) {
@@ -48,12 +77,13 @@ $(function(){
 
         if ($this.data('type') === 'price') {
             var price = $this.find('span').hasClass('fa-angle-down')?2:1
+            data.price = price
         }
         if ($this.data('type') === 'num') {
             var num = $this.find('span').hasClass('fa-angle-down')?2:1
+            data.num = num 
         }
-        data.price = price
-        data.num = num 
+        
         render()
 
     })
